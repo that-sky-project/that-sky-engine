476cbd6ec19d5a459fc40af281001a584e062520
HTMonkeyG <3254339747@qq.com>
2026-04-26 19:01:44 +0800

    Base: Add more overrides for StlAllocator.

--- src/Code/Base/StlAllocator.hpp
+++ src/Code/Base/StlAllocator.hpp
@@ -8,4 +8,9 @@ void *operator new[](size_t _Size);
 void operator delete(void *_Block) noexcept;
 void operator delete[](void *_Block) noexcept;
 
+void *operator new(size_t _Size, const std::nothrow_t &_Tag) noexcept;
+void *operator new[](size_t _Size, const std::nothrow_t &_Tag) noexcept;
+void operator delete(void *_Block, const std::nothrow_t &_Tag) noexcept;
+void operator delete[](void *_Block, const std::nothrow_t &_Tag) noexcept;
+
 #endif
